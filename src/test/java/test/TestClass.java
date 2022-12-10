67d7add9ccb33b09aace8738f3ad37085494ef64
Bora-Se <114330033+Bora-Se@users.noreply.github.com>
2022-12-10 09:51:51 +0100

    10.12

---
+++ src/test/java/test/TestClass.java
@@ -0,0 +1,26 @@
+package test;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+        RestApiUtils restApiUtils = new RestApiUtils();
+
+
+    @Test
+    public void verifyUserWithID() {
+        //JSONPATH ile dogruilama yapalim
+        JsonPath json = restApiUtils.checkUserWithIDGet(114351).jsonPath();
+        Assert.assertEquals("Della", json.get("firstName"));
+        Assert.assertEquals("Heaney", json.get("lastName"));
+        Assert.assertEquals("ricardo.larkin@yahoo.com", json.get("email"));
+    }
+
+    @Test
+    public void createUserTestPost() {
+        restApiUtils.postUserApiCall("Erva","3500","37").prettyPrint() ;
+    }
+
+}
