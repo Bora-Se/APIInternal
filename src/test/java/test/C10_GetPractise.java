544ea2594da8ff310b0ac8c7a5a24cb950fbe082
Bora-Se <114330033+Bora-Se@users.noreply.github.com>
2022-12-18 20:33:44 +0100

    C0

---
+++ src/test/java/test/C10_GetPractise.java
@@ -0,0 +1,26 @@
+package test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class C10_GetPractise {
+
+    /*
+            http://dummy.restapiexample.com/api/v1/employees url'ine bir GET request yolladigimizda
+            donen Response'in
+            status code'unun 200,
+            ve content type'inin Aplication.JSON,
+            ve response body'sindeki
+                employees sayisinin 24
+                ve employee'lerden birinin "Ashton Cox"
+                ve girilen yaslar icinde 61,40 ve 30 degerlerinin oldugunu test edin.
+     */
+
+
+
+}
